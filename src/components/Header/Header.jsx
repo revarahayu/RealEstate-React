e3529c784818665e87917b2b06cb81fc0d6c9650
RERE <r3paarahayu@gmail.com>
2024-09-02 13:42:44 +0700

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
+
+const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // Tambahkan event listener saat scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const getMenuStyles = (menuOpened) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpened && "-100%" };
+    }
+  };
+
+  return (
+    <section className={`header ${isScrolled ? "scrolled" : ""}`}>
+      <div className="flexCenter paddings innerWidth h-container">
+        <img src="./logo.png" alt="logo" width={100} />
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setMenuOpened(false);
+          }}
+        >
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+            <a href="#residencies">Residencies</a>
+            <a href="#our-value">Our Value</a>
+            <a href="#contact-us">Contact Us</a>
+            <a href="#get-started">Get Started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
